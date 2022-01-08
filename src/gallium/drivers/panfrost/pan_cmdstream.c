aerisarn/mesa-uwp

commit 79356b2e5f1762aeaaa81cc148cd5b469a47713d

panfrost: Skip rasterizer discard draws without side effects
Minor optimization, but more importantly fixes an interaction of IDVS
with rasterizer discard.

Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14154>

src/gallium/drivers/panfrost/pan_cmdstream.c
@@ -3106,6 +3106,22 @@ panfrost_draw_vbo(struct pipe_context *pipe,
         /* Do some common setup */
         struct panfrost_batch *batch = panfrost_get_batch_for_fbo(ctx);
 
+        /* If rasterization discard is enabled but the vertex shader does not
+         * have side effects (including transform feedback), skip the draw
+         * altogether. This is always an optimization. Additionally, this is
+         * required for Index-Driven Vertex Shading, since IDVS always
+         * rasterizes. The compiler will not use IDVS if the vertex shader has
+         * side effects. So the only problem case is rasterizer discard with a
+         * shader without side effects -- but these draws are useless.
+         */
+        if (panfrost_batch_skip_rasterization(batch)) {
+                struct panfrost_shader_state *vs =
+                        panfrost_get_shader_state(ctx, PIPE_SHADER_VERTEX);
+
+                if (!vs->info.writes_global)
+                        return;
+        }
+
         /* Don't add too many jobs to a single batch. Hardware has a hard limit
          * of 65536 jobs, but we choose a smaller soft limit (arbitrary) to
          * avoid the risk of timeouts. This might not be a good idea. */

anonymususer04/panfork-mesa-22

commit c8950b609e0cadbe1d44ee2ec167c729e0cf4f5b

[#5839][Bug] panfrost: Update viewport before checking if to skip rasterization
panfrost_emit_viewport sets the value of scissor_culls_everything,
which is used to decide whether or not to proceed with a draw. But
this function will not be called if skip_rasterization decides not to
continue with the draw, and so after one draw is skipped, every
following one is skipped until the end of the batch.

To fix this, call panfrost_emit_viewport if we need to before checking
whether to skip the draw. To avoid repeating this needlessly if there
are many skipped draws in a row, clear the dirty bits from
ctx->dirty. However, other code relies on the dirty bits still being
set, so add new dirty flags and set both when marking the state dirty.

(The odd ordering of the bits in the dirty enum, with the two DSC
entries next to each other, saves a couple of mov instructions on
AArch64, but isn't very important.)

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/5839
Fixes: 79356b2e5f1 ("panfrost: Skip rasterizer discard draws without side effects")

src/gallium/drivers/panfrost/pan_cmdstream.c
@@ -2614,7 +2614,7 @@ panfrost_update_state_3d(struct panfrost_batch *batch)
 {
         unsigned dirty = batch->ctx->dirty;
 
-        if (dirty & (PAN_DIRTY_VIEWPORT | PAN_DIRTY_SCISSOR))
+        if (dirty & (PAN_DIRTY_VIEWPORT_DSC | PAN_DIRTY_SCISSOR_DSC))
                 batch->viewport = panfrost_emit_viewport(batch);
 
         if (dirty & PAN_DIRTY_TLS_SIZE)
@@ -3120,6 +3120,13 @@ panfrost_draw_vbo(struct pipe_context *pipe,
          * side effects. So the only problem case is rasterizer discard with a
          * shader without side effects -- but these draws are useless.
          */
+
+        /* Updating the viewport may change what skip_rasterization decides */
+        if (ctx->dirty & (PAN_DIRTY_VIEWPORT_DSC | PAN_DIRTY_SCISSOR_DSC)) {
+                batch->viewport = panfrost_emit_viewport(batch);
+                ctx->dirty &= ~(PAN_DIRTY_VIEWPORT_DSC | PAN_DIRTY_SCISSOR_DSC);
+        }
+
         if (panfrost_batch_skip_rasterization(batch)) {
                 struct panfrost_shader_state *vs =
                         panfrost_get_shader_state(ctx, PIPE_SHADER_VERTEX);

src/gallium/drivers/panfrost/pan_context.c
@@ -228,7 +228,7 @@ panfrost_bind_rasterizer_state(
          * dependencies are too intricate to bother tracking in detail. However
          * we could probably diff the renderers for viewport dirty tracking,
          * that just cares about the scissor enable and the depth clips. */
-        ctx->dirty |= PAN_DIRTY_SCISSOR;
+        ctx->dirty |= PAN_DIRTY_SCISSOR | PAN_DIRTY_SCISSOR_DSC;
         ctx->dirty_shader[PIPE_SHADER_FRAGMENT] |= PAN_DIRTY_STAGE_RENDERER;
 }
 
@@ -759,7 +759,7 @@ panfrost_set_viewport_states(struct pipe_context *pipe,
         assert(num_viewports == 1);
 
         ctx->pipe_viewport = *viewports;
-        ctx->dirty |= PAN_DIRTY_VIEWPORT;
+        ctx->dirty |= PAN_DIRTY_VIEWPORT | PAN_DIRTY_VIEWPORT_DSC;
 }
 
 static void
@@ -774,7 +774,7 @@ panfrost_set_scissor_states(struct pipe_context *pipe,
         assert(num_scissors == 1);
 
         ctx->scissor = *scissors;
-        ctx->dirty |= PAN_DIRTY_SCISSOR;
+        ctx->dirty |= PAN_DIRTY_SCISSOR | PAN_DIRTY_SCISSOR_DSC;
 }
 
 static void

src/gallium/drivers/panfrost/pan_context.h
@@ -59,15 +59,18 @@ struct prim_convert_context;
 
 /* Dirty tracking flags. 3D is for general 3D state. Shader flags are
  * per-stage. Renderer refers to Renderer State Descriptors. Vertex refers to
- * vertex attributes/elements. */
+ * vertex attributes/elements. _DSC flags must be set alongside the
+ * corresponding unmarked flag, and may be cleared before the main flag is. */
 
 enum pan_dirty_3d {
         PAN_DIRTY_VIEWPORT       = BITFIELD_BIT(0),
-        PAN_DIRTY_SCISSOR        = BITFIELD_BIT(1),
-        PAN_DIRTY_VERTEX         = BITFIELD_BIT(2),
-        PAN_DIRTY_PARAMS         = BITFIELD_BIT(3),
-        PAN_DIRTY_DRAWID         = BITFIELD_BIT(4),
-        PAN_DIRTY_TLS_SIZE       = BITFIELD_BIT(5),
+        PAN_DIRTY_VIEWPORT_DSC   = BITFIELD_BIT(1),
+        PAN_DIRTY_SCISSOR_DSC    = BITFIELD_BIT(2),
+        PAN_DIRTY_SCISSOR        = BITFIELD_BIT(3),
+        PAN_DIRTY_VERTEX         = BITFIELD_BIT(4),
+        PAN_DIRTY_PARAMS         = BITFIELD_BIT(5),
+        PAN_DIRTY_DRAWID         = BITFIELD_BIT(6),
+        PAN_DIRTY_TLS_SIZE       = BITFIELD_BIT(7),
 };
 
 enum pan_dirty_shader {
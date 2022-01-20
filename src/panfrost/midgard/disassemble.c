Grima04/mesa-turnip-kgsl

commit b833702cc1b53cb6e0f0e486a56aa62d9650e79b

pan/mdg: Print constant vectors less wrong
For !32-bit types, we need to pay attention to rep_low/high/half to
determine the effective swizzle.

Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5151>

src/panfrost/midgard/disassemble.c
@@ -365,16 +365,24 @@ print_vector_constants(FILE *fp, unsigned src_binary,
         comp_mask = effective_writemask(alu, condense_writemask(alu->mask, bits));
         num_comp = util_bitcount(comp_mask);
 
-        fprintf(fp, "#");
-        if (num_comp > 1)
-                fprintf(fp, "vec%d(", num_comp);
-
+        fprintf(fp, "<");
         bool first = true;
 
 	for (unsigned i = 0; i < max_comp; ++i) {
                 if (!(comp_mask & (1 << i))) continue;
 
                 unsigned c = (src->swizzle >> (i * 2)) & 3;
+
+                if (bits == 16 && !src->half) {
+                        if (i < 4)
+                                c += (src->rep_high * 4);
+                        else
+                                c += (!src->rep_low * 4);
+                } else if (bits == 32 && !src->half) {
+                        /* Implicitly ok */
+                } else {
+                        printf(" (%d%d%d)", src->rep_low, src->rep_high, src->half);
+                }
 
                 if (first)
                         first = false;
@@ -386,7 +394,7 @@ print_vector_constants(FILE *fp, unsigned src_binary,
         }
 
         if (num_comp > 1)
-                fprintf(fp, ")");
+                fprintf(fp, ">");
 }
 
 static void

anonymususer04/panfork-mesa-22

commit 2cb87ed90971a26fff748a45635ece5f03274aad

[Bug] pan/mdg: Fix usage of printf instead of fprintf in disassembler
Fixes: b833702cc1b ("pan/mdg: Print constant vectors less wrong")

src/panfrost/midgard/disassemble.c
@@ -622,7 +622,7 @@ print_vector_constants(FILE *fp, unsigned src_binary,
                         if (i & 1)
                                 c++;
                 } else {
-                        printf(" (%u)", src->expand_mode);
+                        fprintf(fp, " (%u)", src->expand_mode);
                 }
 
                 if (first)
79c8c037867b92617d949225fba524b122696f83
Icecream95 <ixn@disroot.org>
2022-02-07 19:23:22 +1300

    [TexBO] nir: Add pass to lower buffer textures to 2D textures
    Allows creating larger buffer textures on hardware with limited 1D
    texture support.

--- src/compiler/nir/nir_lower_tex.c
+++ src/compiler/nir/nir_lower_tex.c
@@ -230,6 +230,34 @@ lower_rect_tex_scale(nir_builder *b, nir_tex_instr *tex)
 }
 
 static void
+lower_buffer(nir_builder *b, nir_tex_instr *tex)
+{
+   b->cursor = nir_before_instr(&tex->instr);
+
+   int coord_index = nir_tex_instr_src_index(tex, nir_tex_src_coord);
+
+   nir_src *coord_src = &tex->src[coord_index].src;
+
+   if (coord_index != -1 && nir_src_bit_size(*coord_src) > 16) {
+      assert(tex->coord_components == 1);
+      tex->coord_components = 2;
+
+      /* Use u2u32 because hadware usually expects signed values, so keeping
+       * it 16-bit won't work. */
+      nir_ssa_def *coord = nir_ssa_for_src(b, *coord_src, 1);
+      nir_ssa_def *txs = nir_get_texture_size(b, tex);
+
+      coord = nir_umin(b, coord, txs);
+
+      nir_instr_rewrite_src(&tex->instr, coord_src,
+                            nir_src_for_ssa(nir_u2u32(b, nir_unpack_32_2x16(b, coord))));
+   }
+
+   /* Set this at the end so the txs has a single component */
+   tex->sampler_dim = GLSL_SAMPLER_DIM_2D;
+}
+
+static void
 lower_lod(nir_builder *b, nir_tex_instr *tex, nir_ssa_def *lod)
 {
    assert(tex->op == nir_texop_tex || tex->op == nir_texop_txb);
@@ -1340,6 +1368,14 @@ nir_lower_tex_block(nir_block *block, nir_builder *b,
             lower_rect(b, tex);
          else
             lower_rect_tex_scale(b, tex);
+
+         progress = true;
+      }
+
+      if ((tex->sampler_dim == GLSL_SAMPLER_DIM_BUF) &&
+          options->lower_tex_buffer && tex->op == nir_texop_txf) {
+
+         lower_buffer(b, tex);
 
          progress = true;
       }

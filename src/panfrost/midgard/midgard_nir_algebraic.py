52f605a9a86bf81d481c227cb60f8d1ed79297e6
Icecream95 <ixn@disroot.org>
2022-02-10 23:46:16 +1300

    [OpenCL][Bug] pan/mdg: Fix clz of 8-bit values
    We can't cast straight to 32 bits, so need a u2u16 before the u2u32
    op. The lowering for clz is done after conversions are handled, so
    this won't be automatically dealt with.
    
    TODO: Could we instead cast to 16 bits and leave it there, so the
    16-bit -> 32-bit cast is done as a source expansion?

--- src/panfrost/midgard/midgard_nir_algebraic.py
+++ src/panfrost/midgard/midgard_nir_algebraic.py
@@ -75,7 +75,7 @@ algebraic_late = [
 # lowering in MIR would be painful without a competent builder, so eat the
 # extra instruction
 for sz in ('8', '16', '32'):
-    converted = ('u2u32', a) if sz != '32' else a
+    converted = ('u2u32', (a if sz != '8' else ('u2u16', a))) if sz != '32' else a
     algebraic_late += [(('ufind_msb', 'a@' + sz), ('isub', 31, ('uclz', converted)))]
 
 # Midgard is able to type convert down by only one "step" per instruction; if
